9516e4df4ccad65fe06c91a7911493fbd97bca8d
Steve Beaugé <stevegit@outlook.com>
2017-10-12 13:28:40 +0200

    Better support for multi version dependency

--- Src/SoSP.PnPProvisioningExtensions/SoSP.PnPProvisioningExtensions.Core/debug.15.ps1
+++ Src/SoSP.PnPProvisioningExtensions/SoSP.PnPProvisioningExtensions.Core/debug.15.ps1
@@ -1,24 +1,21 @@
-$moduleAssembly = [System.Reflection.Assembly]::LoadFrom("$([Environment]::GetFolderPath('MyDocuments'))\WindowsPowerShell\Modules\SharePointPnPPowerShell2013\OfficeDevPnP.Core.dll")
+$moduleAssembly = [System.Reflection.Assembly]::LoadFrom("$pwd\SharePointPnP.PowerShell.2013.Commands.dll")
 
 Import-Module -Assembly $moduleAssembly
 
-Connect-PnPOnline -CurrentCredentials http://sbdev02/sites/pnpsrc03
-
 Add-Type -Path SoSP.PnPProvisioningExtensions.Core.dll
 
-
+<#
 $listContentConfig = '[
 {
     "ListName":  "Une liste avec espaces",
     "KeyFieldName":  "Title",
     "UpdateBehavior":  0
 }
-]'
+]'#>
 
 $extHandlers = @(
     New-PnPExtensbilityHandlerObject -Type SoSP.PnPProvisioningExtensions.Core.ListContentHandler `
-                                     -Assembly "SoSP.PnPProvisioningExtensions.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" `
-                                     -Configuration $listContentConfig
+                                     -Assembly "SoSP.PnPProvisioningExtensions.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" 
     New-PnPExtensbilityHandlerObject -Type SoSP.PnPProvisioningExtensions.Core.MetadataNavigationHandler `
                                      -Assembly "SoSP.PnPProvisioningExtensions.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"
     New-PnPExtensbilityHandlerObject -Type SoSP.PnPProvisioningExtensions.Core.DocumentSetHomePageHandler `
